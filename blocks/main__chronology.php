00928207db29455d3bac91e4853d5da58939e4ea
design4people <>
2022-02-18 00:50:49 +0200

    Update main__chronology.php

--- blocks/main__chronology.php
+++ blocks/main__chronology.php
@@ -1,15 +1,12 @@
 <h1 class="page__title"><?php echo $myrow_set['title']; ?></h1>
 <p class="page__description"><?php echo $myrow_set['description']; ?></p>
-
 <div class="page-main__container">
-  <div class="page-main__banner"></div>
-  <div class="banner__468-60--ubn none">
-    <?php include("banner__468-60--ubn.php"); ?>
+  <div class="page-main__banner none"></div>
+  <div class="page-main__banner--midle">
+    <?php include("banner__main__middle.php"); ?>
   </div>
 </div>
-
 <div class="section__caption">#<?php echo $myrow_set['title_s']; ?></div>
-
 <section class="compositions">
   <!-- list -->
   <ul class="compositions__list">
