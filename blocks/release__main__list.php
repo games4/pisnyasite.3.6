9e0d4bffb6c6ea91a4676d58191dd18497f0adca
design4people <>
2023-02-13 17:16:56 +0200

    add DISTINCT statement to SELECT
    Відбираю унікальні записи за допомогою оператора
    DISTINCT

--- blocks/release__main__list.php
+++ blocks/release__main__list.php
@@ -1,5 +1,5 @@
 <?php 
-$result_composition = mysql_query ("SELECT vydannya.title AS vydannya, kompozycija.id, kompozycija.title, vykonavec.title AS vykonavec FROM vydannya, kompozycija, view, vykonavec WHERE vydannya.id = $id AND kompozycija.vydannya = vydannya.id AND view.kompozycija = kompozycija.id AND kompozycija.vykonavec1 = vykonavec.id ORDER BY kompozycija.title", $db);
+$result_composition = mysql_query ("SELECT DISTINCT vydannya.title AS vydannya, kompozycija.id, kompozycija.title, vykonavec.title AS vykonavec FROM vydannya, kompozycija, view, vykonavec WHERE vydannya.id = $id AND kompozycija.vydannya = vydannya.id AND view.kompozycija = kompozycija.id AND kompozycija.vykonavec1 = vykonavec.id ORDER BY kompozycija.title", $db);
 verification_query_2($result_composition);
 
 while ($myrow_composition = mysql_fetch_array($result_composition))
