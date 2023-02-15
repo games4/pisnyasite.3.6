a623d545d0f1565107d4ccc451ed68ca77698875
design4people <>
2023-02-15 18:04:36 +0200

    Update main__index.php

--- blocks/main__index.php
+++ blocks/main__index.php
@@ -13,32 +13,30 @@
   }
 ?>
 
-<div class="section__caption">#Наша сімка </div>
-<section class="compositions">
-  <h3 class="visually-hidden">Cімка попередніх комозицій сайту</h3>
-  <ul class="compositions__seven">
-    <?php include("compositions__list-seven.php"); ?>
-  </ul>
-  <p class="the-end">* * *</p>
-</section>
-<div class="page-main__container">
-  <div class="page-main__banner none"></div>
-  <div class="page-main__banner--midle">
-    <?php include("banner__main__middle.php"); ?>
-  </div>
-</div>
-<div class="section__caption">#Композиція</div>
-<article class="composition" id="composition">
-  <div class="composition__video">
-    <?php include("composition__video.php"); ?>
-  </div>
-  <header class="composition__info">
-    <?php include("composition__info.php"); ?>
-  </header>
-  <section class="composition__lyric lyric">
-    <h3 class="lyric__title"><?php echo $myrow['title']; ?></h3>
-    <?php if ( trim($myrow_tvir['ftext']) == "" ) { echo '<p>'.nl2br(trim($myrow_tvir['text'])).'</p>' ;} else { echo trim($myrow_tvir['ftext']) ;} ?>
-    <p class="the-end">* * *</p>
-  </section>
-  <?php include("composition__release.php"); ?>
-</article>
+        <div class="section__caption">#Наша сімка </div>
+        <section class="compositions">
+          <h3 class="visually-hidden">Cімка попередніх комозицій сайту</h3>
+          <?php include("compositions__list-seven.php"); ?>
+          <p class="the-end">* * *</p>
+        </section>
+        <div class="page-main__container">
+          <div class="page-main__banner none"></div>
+          <div class="page-main__banner--midle">
+            <?php include("banner__main__middle.php"); ?>
+          </div>
+        </div>
+        <div class="section__caption">#Композиція</div>
+        <article class="composition" id="composition">
+          <div class="composition__video">
+            <?php include("composition__video.php"); ?>
+          </div>
+          <header class="composition__info">
+            <?php include("composition__info.php"); ?>
+          </header>
+          <section class="composition__lyric lyric">
+            <h3 class="lyric__title"><?php echo $myrow['title']; ?></h3>
+            <?php if ( trim($myrow_tvir['ftext']) == "" ) { echo '<p>'.nl2br(trim($myrow_tvir['text'])).'</p>' ;} else { echo trim($myrow_tvir['ftext']) ;} ?>
+            <p class="the-end">* * *</p>
+          </section>
+          <?php include("composition__release.php"); ?>
+        </article>
