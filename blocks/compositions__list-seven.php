a49665daf37dc8fae2fb56018da9a2ad01744631
design4people <>
2023-02-15 18:11:52 +0200

    add tag <span> for "compositions__title"

--- blocks/compositions__list-seven.php
+++ blocks/compositions__list-seven.php
@@ -3,6 +3,7 @@ $num = $myrow["num"];
 $count = mysql_query ("SELECT num FROM view WHERE num < $num ORDER BY date DESC, num DESC");
 verification_query_2($count);
 
+echo '<ul class="compositions__list">';
 if (mysql_num_rows($count) < 7)
   {
     $result_k = mysql_query ("SELECT kompozycija.id, kompozycija.title, kompozycija.header, view.date, vykonavec.title AS vykonavec FROM view, kompozycija, vykonavec WHERE kompozycija.id = view.kompozycija AND kompozycija.vykonavec1 = vykonavec.id ORDER BY view.date, view.num LIMIT 7",$db); 
@@ -29,10 +30,13 @@ else
       echo '
     <li>
       <a class="compositions__link" href="kompozycija.php?id='.$myrow_k["id"].'">
-        <time class="compositions__date" datetime="'.$myrow_k["date"].'">'.$myrow_k["date"].'</time>
-        <b class="compositions__title">'.$myrow_k["header"].'</b>
+        <span><time class="compositions__date" datetime="'.$myrow_k["date"].'">'.$myrow_k["date"].'</time></span>
+        <span><b class="compositions__title">'.$myrow_k["header"].'</b></span>
       </a>
     </li>';
     }
   }
+echo '
+  </ul>
+';
 ?>
