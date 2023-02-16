e9f55b6f63c85bfff731fca1985a36de4446b398
design4people <>
2023-02-16 23:28:22 +0200

    Create main__videos.php

---
+++ blocks/main__videos.php
@@ -0,0 +1,15 @@
+<h1 class="page__title"><?php echo $myrow_set['title']; ?></h1>
+<p class="page__description"><?php echo $myrow_set['description']; ?></p>
+<div class="page-main__container">
+  <div class="page-main__banner none"></div>
+  <div class="page-main__banner--midle">
+    <?php include("banner__main__middle.php"); ?>
+  </div>
+</div>
+<div class="section__caption">#<?php echo $myrow_set['title_s']; ?></div>
+<section class="compositions">
+  <!-- list -->
+  <?php include("main__videos__list.php");?>
+  <p class="the-end">* * *</p>
+  <!-- /list -->
+</section>
