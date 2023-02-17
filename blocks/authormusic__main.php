d788b16d48368f860e73b60691d5e00db336688d
design4people <>
2023-02-17 14:21:35 +0200

    Create authormusic__main.php

---
+++ blocks/authormusic__main.php
@@ -0,0 +1,18 @@
+<h1 class="page__title"><span class="page__label">Автор музики </span><?php echo ' '.mb_ucfirst($myrow["title"]); ?></h1>
+<? 
+if ($myrow_set['description'] != "")
+{
+  echo '<p class="page__description">'.$myrow_set['description'].'</p>';
+}
+?>
+<div class="section__caption">#Композиції</div>
+<section class="compositions">
+  <?php include("authormusic__main__content.php"); ?>
+  <p class="the-end">* * *</p>
+</section>
+<div class="page-main__container">
+  <div class="page-main__banner none"></div>
+  <div class="page-main__banner--midle">
+    <?php include("banner__main__middle.php"); ?>
+  </div>
+</div>
