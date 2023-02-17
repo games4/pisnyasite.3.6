a35fa01aa0bef77ec8b20a0c8357f56e9b7fb697
design4people <>
2023-02-17 16:56:20 +0200

    Create search__main.php

---
+++ blocks/search__main.php
@@ -0,0 +1,22 @@
+<article class="release">
+  <h1 class="page__title">Пошук по сайту</h1>
+  <!--<p class="section__caption">#Результати пошуку</p>-->
+  <!--<section class="release__info">-->
+  <!--</section>-->
+  <section class="release__compositions compositions">
+    <p class="section__caption">#Результати</p>
+    <div>
+      <?php include("search__main__content.php"); ?>
+    </div>
+    <ul class="compositions__list">
+      <?php // include("release__main__content.php"); ?>
+    </ul>
+    <div class="page-main__container">
+      <div class="page-main__banner none"></div>
+      <div class="page-main__banner--midle">
+        <?php include("banner__main__middle.php"); ?>
+      </div>
+    </div>
+    <p class="the-end">* * *</p>
+  </section>
+</article>
