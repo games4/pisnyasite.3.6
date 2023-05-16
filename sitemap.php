c6c5fefc424d5182c90ecc276512fec852fc860d
design4people <games4@ukr.net>
2023-05-17 02:38:36 +0300

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -3,15 +3,15 @@ include_once("blocks/func.inc.php");
 include("blocks/db.php");
 
 // 2023-03-27
-// визначаю загальну кількість записів в базі даних
-$result00 = mysql_query("SELECT COUNT(*) FROM view");
+// визначаю загальну кількість записів у запиті
+$result00 = mysql_query("SELECT COUNT(DISTINCT kompozycija) FROM view");
 verification_query($result00);
 $temp = mysql_fetch_array($result00);
 $posts = $temp[0];
-// echo "<p class='p1'>всього записів в базі даних view - $posts</p>";
+// echo "<p>всього записів в запиті view_compozition_count - $posts</p>";
 // визначаю загальну кількість сторінок 
 
-$result = mysql_query ("SELECT * FROM view, kompozycija WHERE view.kompozycija = kompozycija.id ORDER BY view.date DESC, view.num DESC", $db);
+$result = mysql_query ("SELECT view.date FROM view, kompozycija WHERE view.kompozycija = kompozycija.id ORDER BY view.date DESC, view.num DESC", $db);
 verification_query($result);
 $myrow = mysql_fetch_array ($result);
 $date = date("Y-m-d", strtotime($myrow["date"]));
@@ -24,20 +24,40 @@ echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>';
+echo '
+  <url>
+    <loc>https://pisnya.org.ua/albomy.php</loc>
+    <lastmod>'.$date.'</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>';
 
-$result_k = mysql_query ("SELECT view.date, kompozycija.id, kompozycija.lastmod_date, kompozycija.header, vykonavec.title AS vykonavec FROM view, kompozycija, vykonavec WHERE kompozycija.id = view.kompozycija AND kompozycija.vykonavec1 = vykonavec.id ORDER BY view.date DESC, view.num DESC", $db); 
-verification_query($result_k);
-
-while ($myrow_k = mysql_fetch_array($result_k))
+$result_kompozycija = mysql_query ("SELECT DISTINCT kompozycija.id, kompozycija.lastmod_date FROM view, kompozycija WHERE kompozycija.id = view.kompozycija ORDER BY view.date DESC", $db); 
+verification_query($result_kompozycija);
+while ($myrow_kompozycija = mysql_fetch_array($result_kompozycija))
 {
   echo '
   <url>
-    <loc>https://pisnya.org.ua/kompozycija.php?id='.$myrow_k["id"].'</loc>
-    <lastmod>'.date("Y-m-d", strtotime($myrow_k["lastmod_date"])).'</lastmod>
+    <loc>https://pisnya.org.ua/kompozycija.php?id='.$myrow_kompozycija["id"].'</loc>
+    <lastmod>'.date("Y-m-d", strtotime($myrow_kompozycija["lastmod_date"])).'</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.9</priority>
   </url>';
 }
+
+$result_vydannya = mysql_query ("SELECT DISTINCT vydannya.id, vydannya.lastmod_date FROM view, kompozycija, vydannya WHERE view.kompozycija = kompozycija.id AND kompozycija.vydannya = vydannya.id ORDER BY vydannya.id DESC", $db); 
+verification_query_2($result_vydannya);
+while ($myrow_vydannya = mysql_fetch_array($result_vydannya))
+{
+  echo '
+  <url>
+    <loc>https://pisnya.org.ua/release.php?id='.$myrow_vydannya["id"].'</loc>
+    <lastmod>'.date("Y-m-d", strtotime($myrow_vydannya["lastmod_date"])).'</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.9</priority>
+  </url>';
+}
+
 echo '
 </urlset>';
 
