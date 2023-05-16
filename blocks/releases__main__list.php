da8f0cb803f450a39dbf4f36df18d042e08e0c65
design4people <games4@ukr.net>
2023-05-16 13:52:09 +0300

    fix releases__main__list.php
    реалізація видачі тільки тих релізів/видань композиції з яких опубліковані на сайті

--- blocks/releases__main__list.php
+++ blocks/releases__main__list.php
@@ -1,27 +1,27 @@
 <?php 
-$result_release = mysql_query ("SELECT DISTINCT vyd FROM vydannya ORDER BY vyd", $db); 
-verification_query_2($result_release);
+$result_release_type = mysql_query ("SELECT DISTINCT type, release_type.id, release_type.name FROM vydannya, release_type WHERE type = release_type.id ORDER BY type", $db); 
+verification_query_2($result_release_type);
 
 echo '<ul class="types__list">';
-while ($myrow_release = mysql_fetch_array($result_release)) 
+while ($myrow_release_type = mysql_fetch_array($result_release_type)) 
 {
-  $release = $myrow_release["vyd"];
-  if ($release != "")
+  $release_type_id = $myrow_release_type["type"];
+  if ($release_type_id != "")
   {
-    $result_k = mysql_query ("SELECT vydannya.id, vydannya.title, vydannya.vyd, vydannya.rik, vydavnyk.title AS vydavnyk FROM vydannya, vydavnyk WHERE vydannya.vydavnyk = vydavnyk.id AND vydannya.vyd = '$release' AND vydannya.vyd != '' ORDER BY vydannya.title",$db); 
-    verification_query_2($result_k);
+    $result_vydannya = mysql_query ("SELECT DISTINCT vydannya.id, vydannya.title, vydannya.rik, vydavnyk.title AS vydavnyk FROM view, kompozycija, vydannya, vydavnyk WHERE view.kompozycija = kompozycija.id AND kompozycija.vydannya = vydannya.id AND vydannya.vydavnyk = vydavnyk.id AND vydannya.type = '$release_type_id' AND vydannya.type != '' ORDER BY vydannya.title", $db); 
+    verification_query_2($result_vydannya);
     echo '
     <li>
-      <h2 class="">'.$release.'</h2>
+      <h2 class="">'.$myrow_release_type["name"].'</h2>
       <ul class="releases__list">';
-    while ($myrow_k = mysql_fetch_array($result_k))
-    {
-      echo '
+      while ($myrow_vydannya = mysql_fetch_array($result_vydannya))
+      {
+        echo '
         <li class="release__item">
-          <a class="relese__link" href="release.php?id='.$myrow_k["id"].'">'.$myrow_k["title"].' / '.$myrow_k["vydavnyk"].', '.$myrow_k["rik"].' рік'.'</a>
+          <a class="relese__link" href="release.php?id='.$myrow_vydannya["id"].'">'.$myrow_vydannya["title"].' / '.$myrow_vydannya["vydavnyk"].', '.$myrow_vydannya["rik"].' рік'.'</a>
         </li>';
-    }
-    echo '
+      }
+      echo '
       </ul>
     </li>';
   }
