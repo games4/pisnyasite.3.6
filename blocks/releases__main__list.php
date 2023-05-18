15b7b9e4ac23680c78996ab027e47d24710b9701
design4people <games4@ukr.net>
2023-05-18 04:08:47 +0300

    Update releases__main__list.php

--- blocks/releases__main__list.php
+++ blocks/releases__main__list.php
@@ -1,5 +1,13 @@
 <?php 
-$result_release_type = mysql_query ("SELECT DISTINCT type, release_type.id, release_type.name FROM vydannya, release_type WHERE type = release_type.id ORDER BY type", $db); 
+// 2023-05-17
+// визначаю загальну кількість записів у запиті
+$result_vydannya_count = mysql_query ("SELECT COUNT(DISTINCT vydannya.id) FROM view, kompozycija, vydannya, vydavnyk WHERE view.kompozycija = kompozycija.id AND kompozycija.vydannya = vydannya.id AND vydannya.vydavnyk = vydavnyk.id AND vydannya.vydavnyk != ''", $db);
+verification_query_2($result_vydannya_count);
+$myrow_vydannya_count = mysql_fetch_array($result_vydannya_count);
+$vydannya_count = $myrow_vydannya_count[0];
+//echo "<p>всього записів у vydannya_count - $vydannya_count</p>";
+
+$result_release_type = mysql_query ("SELECT DISTINCT vydannya.type, release_type.name FROM view, kompozycija, vydannya, release_type WHERE view.kompozycija = kompozycija.id AND kompozycija.vydannya = vydannya.id AND vydannya.type = release_type.id ORDER BY release_type.name ", $db); 
 verification_query_2($result_release_type);
 
 echo '<ul class="types__list">';
@@ -8,7 +16,7 @@ while ($myrow_release_type = mysql_fetch_array($result_release_type))
   $release_type_id = $myrow_release_type["type"];
   if ($release_type_id != "")
   {
-    $result_vydannya = mysql_query ("SELECT DISTINCT vydannya.id, vydannya.title, vydannya.rik, vydavnyk.title AS vydavnyk FROM view, kompozycija, vydannya, vydavnyk WHERE view.kompozycija = kompozycija.id AND kompozycija.vydannya = vydannya.id AND vydannya.vydavnyk = vydavnyk.id AND vydannya.type = '$release_type_id' AND vydannya.type != '' ORDER BY vydannya.title", $db); 
+    $result_vydannya = mysql_query ("SELECT DISTINCT vydannya.id, vydannya.title, vydannya.rik, vydavnyk.title AS vydavnyk FROM view, kompozycija, vydannya, vydavnyk WHERE view.kompozycija = kompozycija.id AND kompozycija.vydannya = vydannya.id AND vydannya.type = '$release_type_id' AND vydannya.vydavnyk = vydavnyk.id ORDER BY vydannya.title", $db); 
     verification_query_2($result_vydannya);
     echo '
     <li>
